6568e86d91cf8a89de7dd511a6c179b4a4dadc95
xushyoq <husnudekubzonov@gmail.com>
2026-02-17 20:17:13 +0500

    Initial create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InventoryManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
