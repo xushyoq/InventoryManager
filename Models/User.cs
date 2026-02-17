d1d0957d459286945524e71fe2487a19e3417ace
xushyoq <husnudekubzonov@gmail.com>
2026-02-17 23:29:10 +0500

    configure program.cs

---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManager.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Email { get; set; }
+
+    [Required]
+    [MaxLength(500)]
+    public string Name { get; set; }
+
+    [MaxLength(500)]
+    public string ProfileImageUrl { get; set; }
+
+    public bool IsAdmin { get; set; } = false;
+    public bool IsBlcoked { get; set; } = false;
+
+    [MaxLength(255)]
+    public string Provider { get; set; }
+
+    [MaxLength(500)]
+    public string ProviderUserId { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DeafaultConnection' not found.");
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClinetSecret"];
+});
 
 var app = builder.Build();
 
@@ -13,11 +31,13 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
