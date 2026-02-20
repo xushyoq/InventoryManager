5444d6562f2b2d6a879d45e1024896bf439814ea
xushyoq <husnudekubzonov@gmail.com>
2026-02-20 23:04:04 +0500

    add appsettings.Production.json

--- obj/Debug/net8.0/InventoryManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventoryManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0047a599dd57c0d9390ea5c485e7a8b368c3e639")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6cf69e371e76f7f245b2deb63b5986251f50fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
