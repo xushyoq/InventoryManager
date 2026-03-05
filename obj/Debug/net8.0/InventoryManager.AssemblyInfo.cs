e6469941646f3e7f25154883963a55eeb078a4f0
xushyoq <husnudekubzonov@gmail.com>
2026-03-06 01:00:10 +0500

    Update homepage

--- obj/Debug/net8.0/InventoryManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventoryManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6212857358c24d2ede1036a0092590ba6cf6348")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf13f3658dcd11e5025d0a3557b9d0566c1a473")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
